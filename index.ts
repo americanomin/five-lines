290dcb265e78d7c6f3e94a194f22f30a44edbba8
americanomin <americanomin@gmail.com>
2023-11-02 22:40:42 +0900

    4.1.4. 클래스로 코드 이관하기
    타입 코드를 볼 때는 항상 즉시 enum (열거형)으로 변환한다.
    int 형의 코드로 관리하면, 코드 사용 추적이 어렵다.

--- index.ts
+++ index.ts
@@ -18,69 +18,30 @@ enum Tile {
 }
 
 interface Input {
-  isRight(): boolean;
-  isLeft(): boolean;
-  isUp(): boolean;
-  isDown(): boolean;
+  handle(): void;
 }
 
 class Right implements Input {
-  isRight() {
-    return true;
-  }
-  isLeft() {
-    return false;
-  }
-  isUp() {
-    return false;
-  }
-  isDown() {
-    return false;
+  handle() {
+    moveHorizontal(1);
   }
 }
 
 class Left implements Input {
-  isRight() {
-    return false;
-  }
-  isLeft() {
-    return true;
-  }
-  isUp() {
-    return false;
-  }
-  isDown() {
-    return false;
+  handle() {
+    moveHorizontal(-1);
   }
 }
 
 class Up implements Input {
-  isRight() {
-    return false;
-  }
-  isLeft() {
-    return false;
-  }
-  isUp() {
-    return true;
-  }
-  isDown() {
-    return false;
+  handle() {
+    moveVertical(1);
   }
 }
 
 class Down implements Input {
-  isRight() {
-    return false;
-  }
-  isLeft() {
-    return false;
-  }
-  isUp() {
-    return false;
-  }
-  isDown() {
-    return true;
+  handle() {
+    moveVertical(-1);
   }
 }
 
@@ -188,15 +149,8 @@ function updateTile(y: number, x: number) {
 function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    handleInput(current);
+    current.handle();
   }
-}
-
-function handleInput(input: Input) {
-  if (input.isLeft()) moveHorizontal(-1);
-  else if (input.isRight()) moveHorizontal(1);
-  else if (input.isUp()) moveVertical(-1);
-  else if (input.isDown()) moveVertical(1);
 }
 
 function draw() {
